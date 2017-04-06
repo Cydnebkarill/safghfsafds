d7a182982e620542b2636cdf94bff2814cfab0d1
Risnar <Matthias@Up-in-the-Air.local>
2017-04-06 17:56:56 +0200

    added first js script to insert a game

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-
+function insertGame(){
+  $.ajax({
+      type: "POST",
+      dataType: "text",
+      url: "insert.php",
+      data: {
+        func: "insertGame",
+        doz: $("#dozent").val()
+      },
+      success: function(data) {
+         alert(data);
+      },
+      error: function(data) {
+        alert("Fehler"+data);
+      }
+    });
+}
